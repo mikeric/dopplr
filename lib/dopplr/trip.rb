0ef679565e988a3421bcc534115a31572cc1121a
Mike Richards <mike22e@gmail.com>
2009-07-29 16:25:24 -0400

    Changed attributes for trip object.

--- lib/dopplr/trip.rb
+++ lib/dopplr/trip.rb
@@ -1,12 +1,24 @@
 module Dopplr
   class Trip
-    def initialize(client, trip_id)
+    attr_reader :start, :finish, :city, :outgoing_transport_type, :return_transport_type, :tags, :notes
+    attr_reader :trip_id, :url
+    
+    def initialize(client, id)
       @client = client
-      @id = trip_id
+      @trip_id = id
+      populate
     end
     
-    def info
-      @client.get 'trip_info', :trip_id => @id
+    def populate
+      info = @client.get('trip_info', :trip_id => @trip_id)['trip']
+      @start                    = info['start']
+      @finish                   = info['finish']
+      @outgoing_transport_type  = info['outgoing_transport_type']
+      @return_transport_type    = info['return_transport_type']
+      @tags                     = info['tag']
+      @notes                    = info['note']
+      @url                      = info['url']
+      @city                     = City.new(@client, info['city']['geoname_id'])
     end
   end
 end

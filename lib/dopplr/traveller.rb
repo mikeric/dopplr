d1e137163feece1395e4b38a42f6bbaca89e3ef7
Mike Richards <mike22e@gmail.com>
2009-08-03 20:42:20 -0400

    Added methods for accessing fellows and shared travellers.

--- lib/dopplr/traveller.rb
+++ lib/dopplr/traveller.rb
@@ -1,7 +1,6 @@
 module Dopplr
   class Traveller
-    attr_reader :nick, :name, :status, :trips, :travel_today, :home_city, :current_city
-    attr_reader :icon_id, :email_sha1, :url
+    attr_reader :nick, :name, :status, :travel_today, :icon_id, :email_sha1, :url
     
     def initialize(client, user = nil)
       @client = client
@@ -15,20 +14,10 @@ module Dopplr
       @nick         = info['nick']
       @name         = info['name']
       @status       = info['status']
+      @travel_today = info['travel_today']
       @icon_id      = info['icon_id']
       @email_sha1   = info['sha1email']
       @url          = info['url']
-      @travel_today = info['travel_today']
-    end
-    
-    def trips(options = {})
-      unless @trips || options[:force]
-        trips = @client.get('trips_info', @params)['trip']
-        @trips = trips.map do |trip|
-          Trip.new @client, trip['id'] unless trips.empty?
-        end
-      end
-      @trips
     end
     
     def current_city(options = {})
@@ -45,6 +34,43 @@ module Dopplr
         @home_city = City.new @client, info['home_city']['geoname_id']
       end
       @home_city
+    end
+    
+    def trips(options = {})
+      unless @trips || options[:force]
+        trips = @client.get('trips_info', @params)['trip']
+        if !trips.empty?
+          @trips = trips.map do |trip|
+            Trip.new @client, trip['id']
+          end
+        end
+      end
+      @trips ||= []
+    end
+    
+    def fellows(options = {})
+      unless @fellows && @sharing || options[:force]
+        fellows = @client.get('fellows', @params)
+        if !fellows['can_see_trips_of'].empty?
+          @fellows = fellows['can_see_trips_of'].map do |fellow|
+            Traveller.new @client, fellow['nick']
+          end
+        end
+        if !fellows['shows_trips_to'].empty?
+          @sharing = fellows['shows_trips_to'].map do |fellow|
+            hash = fellow.inject({}) do |memo,(k,v)|
+              memo[k.to_sym] = v
+              memo
+            end
+            Struct.new(*hash.keys).new(*hash.values_at(*hash.keys))
+          end
+        end
+      end
+      if options[:sharing]
+        @sharing ||= []
+      else
+        @fellows ||= []
+      end
     end
   end
 end
